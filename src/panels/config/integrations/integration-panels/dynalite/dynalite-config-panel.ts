450d323b9f337c32e7a7a73a4659f834898c4cf4
Ziv <16467659+ziv1234@users.noreply.github.com>
2020-08-29 02:37:30 +0300

    refactored many inputs into one

--- src/panels/config/integrations/integration-panels/dynalite/dynalite-config-panel.ts
+++ src/panels/config/integrations/integration-panels/dynalite/dynalite-config-panel.ts
@@ -53,6 +53,16 @@ class HaPanelConfigDynalite extends LitElement {
 
   private _configured = false;
 
+  private _inputRows = [
+    { name: "name", type: "string" },
+    { name: "host", type: "string" },
+    { name: "port", type: "number" },
+    { name: "fade", type: "number" },
+    { name: "active", type: "list", options: this._activeOptions },
+    { name: "autodiscover", type: "boolean" },
+    { name: "polltimer", type: "number" },
+  ];
+
   protected render(): TemplateResult {
     if (!this._configured) return html``;
     return html`
@@ -70,70 +80,20 @@ class HaPanelConfigDynalite extends LitElement {
         <div class="content">
           <ha-card .header=${this._localStr("description_system")}>
             <div class="card-content">
-              <dynalite-single-row
-                id="dyn-name"
-                inputType="string"
-                shortDesc=${this._localStr("name")}
-                longDesc=${this._localStr("name_long")}
-                .value=${this._name}
-                .changeCallback="${this._handleChange.bind(this)}"
-                .narrow=${this.narrow}
-              ></dynalite-single-row>
-              <dynalite-single-row
-                id="dyn-host"
-                inputType="string"
-                shortDesc=${this._localStr("host")}
-                longDesc=${this._localStr("host_long")}
-                .value=${this._host}
-                .changeCallback="${this._handleChange.bind(this)}"
-                .narrow=${this.narrow}
-              ></dynalite-single-row>
-              <dynalite-single-row
-                id="dyn-port"
-                inputType="number"
-                shortDesc=${this._localStr("port")}
-                longDesc=${this._localStr("port_long")}
-                .value=${this._port}
-                .changeCallback="${this._handleChange.bind(this)}"
-                .narrow=${this.narrow}
-              ></dynalite-single-row>
-              <dynalite-single-row
-                id="dyn-fade"
-                inputType="number"
-                shortDesc=${this._localStr("fade")}
-                longDesc=${this._localStr("fade_long")}
-                .value=${this._fade}
-                .changeCallback="${this._handleChange.bind(this)}"
-                .narrow=${this.narrow}
-              ></dynalite-single-row>
-              <dynalite-single-row
-                id="dyn-active"
-                inputType="list"
-                shortDesc=${this._localStr("active")}
-                longDesc=${this._localStr("active_long")}
-                .options=${this._activeOptions}
-                .value=${this._active}
-                .changeCallback="${this._handleChange.bind(this)}"
-                .narrow=${this.narrow}
-              ></dynalite-single-row>
-              <dynalite-single-row
-                id="dyn-autodiscover"
-                inputType="boolean"
-                shortDesc=${this._localStr("auto_discover")}
-                longDesc=${this._localStr("auto_discover_long")}
-                .value=${this._autodiscover}
-                .changeCallback="${this._handleChange.bind(this)}"
-                .narrow=${this.narrow}
-              ></dynalite-single-row>
-              <dynalite-single-row
-                id="dyn-polltimer"
-                inputType="number"
-                shortDesc=${this._localStr("poll_timer")}
-                longDesc=${this._localStr("poll_timer_long")}
-                .value=${this._polltimer}
-                .changeCallback="${this._handleChange.bind(this)}"
-                .narrow=${this.narrow}
-              ></dynalite-single-row>
+              ${this._inputRows.map(
+                (row) => html`
+                  <dynalite-single-row
+                    id=${`dyn-${row.name}`}
+                    inputType=${row.type}
+                    shortDesc=${this._localStr(row.name)}
+                    longDesc=${this._localStr(row.name + "_long")}
+                    .value=${this["_" + row.name]}
+                    .options=${row.options ? row.options : []}
+                    .changeCallback="${this._handleChange.bind(this)}"
+                    .narrow=${this.narrow}
+                  ></dynalite-single-row>
+                `
+              )}
             </div>
             <div class="card-actions">
               <mwc-button @click=${this._publish}>
